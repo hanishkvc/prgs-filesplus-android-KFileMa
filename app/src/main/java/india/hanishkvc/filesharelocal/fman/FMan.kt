b6ef7420109f32d84f34c942d15e933e45fe3149
hanishkvc <hanishkvc@gmail.com>
2020-07-18 13:08:32 +0530

    FMan fix ups - p1
    Removed the makeDetails dummy test value creation logic, as its no
    longer used.
    
    Removed the ITEM_MAP hash map, as I will be using the ArrayList along
    with the corresponding index into it, so no need for a hash map.
    
    Renamed the createDummyItem to the appropraite createFManItem.

--- app/src/main/java/india/hanishkvc/filesharelocal/fman/FMan.kt
+++ app/src/main/java/india/hanishkvc/filesharelocal/fman/FMan.kt
@@ -22,40 +22,25 @@ object FMan {
      */
     var curPath = "/"
 
-    /**
-     * A map of sample (fman) items, by ID.
-     */
-    val ITEM_MAP: MutableMap<String, FManItem> = HashMap()
-
     init {
         // Start at the root dir
         var iCur = 0
         for (de in Files.list(curPath)) {
-            createDummyItem(iCur, de.normalize(),"dirORfile")
+            createFManItem(iCur, de.normalize(),"dirORfile")
             iCur += 1
         }
     }
 
     private fun addItem(item: FManItem) {
         ITEMS.add(item)
-        ITEM_MAP.put(item.id, item)
     }
 
     public fun loadPath(path: String) {
 
     }
 
-    private fun createDummyItem(position: Int, path: String, type: String): FManItem {
+    private fun createFManItem(position: Int, path: String, type: String): FManItem {
         return FManItem(position.toString(), path, type)
-    }
-
-    private fun makeDetails(position: Int): String {
-        val builder = StringBuilder()
-        builder.append("Details about Item: ").append(position)
-        for (i in 0..position - 1) {
-            builder.append("\nMore details information here.")
-        }
-        return builder.toString()
     }
 
     /**
