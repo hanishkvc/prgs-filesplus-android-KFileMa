912c643b685b2c02048cf28579e16e5964e2c159
hanishkvc <hanishkvc@gmail.com>
2020-07-20 19:24:44 +0530

    FManRecyclerViewAdapter: member name updates, id to type
    Have renamed the members of the ViewHolder to represent what they
    will hold.
    
    content renamed to path, bcas it holds the path info.
    	TODO: will be showing only the last part of the path.
    
    id renamed to type. And FMan's type is assigned to it now.
    	TODO: will be adding back id as a non gui widget member.

--- app/src/main/java/india/hanishkvc/filesharelocal/FManRecyclerViewAdapter.kt
+++ app/src/main/java/india/hanishkvc/filesharelocal/FManRecyclerViewAdapter.kt
@@ -1,11 +1,10 @@
 package india.hanishkvc.filesharelocal
 
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-
+import androidx.recyclerview.widget.RecyclerView
 import india.hanishkvc.filesharelocal.fman.FMan.FManItem
 
 /**
@@ -24,18 +23,18 @@ class FManRecyclerViewAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
-        holder.idView.text = item.id
-        holder.contentView.text = item.path
+        holder.typeView.text = item.type
+        holder.pathView.text = item.path
     }
 
     override fun getItemCount(): Int = values.size
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val idView: TextView = view.findViewById(R.id.item_number)
-        val contentView: TextView = view.findViewById(R.id.content)
+        val typeView: TextView = view.findViewById(R.id.item_number)
+        val pathView: TextView = view.findViewById(R.id.content)
 
         override fun toString(): String {
-            return super.toString() + " '" + contentView.text + "'"
+            return super.toString() + " '" + pathView.text + "'"
         }
     }
 }
