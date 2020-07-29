83378f66249733530d52764cd798b5d839755947
hanishkvc <hanishkvc@gmail.com>
2020-07-29 16:35:36 +0530

    SimpRecycView:P1: An attempt at a simple-all-in RecyclerView
    For simple situations, where there is a simple String array that we
    want to show to user in a efficient and easy way, trying to create
    this all-in-one recycler view, which includes the ViewAdapter and
    the ViewHolder within itself, so that there is no need to define
    them seperately, unlike the regular RecyclerView.
    
    As I am new to kotlin and havent looked at intricacies of android's
    gui framework for a long time now, so not sure things are fine yet,
    nor have I looked into the documents, just giving it a try.
    
    THis is the initial skeleton.
    
    There are some kotlin mechanisms that I am not sure yet, like why
    
    * I require to specifiy the dataList as val/var explicit as part of
    the class definition, for me to acces from its own member functions.
    
    * Or why I cant access the dataList in the outer class directly.
    May be I require to use a special tag like outer or so, no idea yet.

---
+++ app/src/main/java/india/hanishkvc/filesharelocal/SimpRecycView.kt
@@ -0,0 +1,46 @@
+/**
+ * SimpRecycView - A simple recycler view based list/grid view which provides the
+ * view adaptor and holder classes within itself.
+ * This is useful if one is working with simple list of strings or so. And just wants
+ * to show the same on the screen without breaking head about too many things.
+ * HanishKVC, 2020
+ * GPL
+ */
+
+package india.hanishkvc.filesharelocal
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class SimpRecycView(context: Context, dataList: ArrayList<String>) : RecyclerView(context) {
+    init {
+        layoutManager = LinearLayoutManager(context)
+        adapter = SimpViewAdapter(dataList)
+    }
+
+    inner class SimpViewAdapter(val dataList: ArrayList<String>) : RecyclerView.Adapter<SimpViewAdapter.SimpViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimpViewHolder {
+            val view = TextView(context)
+            return SimpViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: SimpViewHolder, position: Int) {
+            holder.id = position
+            (holder.itemView as TextView).text = dataList[position]
+        }
+
+        override fun getItemCount(): Int {
+            return dataList.size
+        }
+
+        inner class SimpViewHolder(itemView: View) : ViewHolder(itemView) {
+            var id = -1
+        }
+
+    }
+}
