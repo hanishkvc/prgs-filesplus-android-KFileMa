162f7a33ac43c81afa61bb396494fd871d1f0de5
hanishkvc <hanishkvc@gmail.com>
2020-08-02 16:53:39 +0530

    HPopupMenu bit more documentation

--- app/src/main/java/india/hanishkvc/filesharelocal/HPopupMenu.kt
+++ app/src/main/java/india/hanishkvc/filesharelocal/HPopupMenu.kt
@@ -1,9 +1,5 @@
 /**
  * HPopupMenu - Hierarchical popup menu for android
- * As Android and ANdroidX PopupMenu seems to have issue with Hierarchical menus
- * i.e menu with submenus in it, This helper class to provide same functionality
- * has been created.
- *
  * @author C Hanish Menon <hanishkvc@gmail.com>
  * @version v20200802IST1604
  */
@@ -14,6 +10,20 @@ import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.widget.PopupMenu
 
+/**
+ * HPopupMenu - Hierarchical popup menu for android
+ * As Android and ANdroidX PopupMenu seems to have issue with Hierarchical menus
+ * i.e menu with submenus in it, This helper class to provide same functionality
+ * has been created.
+ *
+ * Android(X) generates
+ *     java.lang.ClassCastException: android.widget.HeaderViewListAdapter cannot be cast to androidx.appcompat.view.menu.MenuAdapter
+ * This works around it by, handling the menu hierarchy has independent menus
+ *
+ * Define the Menu Hierarchy
+ * Load the root Menu
+ * Show the menu
+ */
 
 class HPopupMenu(context: Context, view: View ) {
 
